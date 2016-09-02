3f85c0d93832a1ac52342236ceb925b540eecd7c
Niall Robinson <niall.h.robinson@gmail.com>
2016-09-02 11:17:03 +0100

    initial import

---
+++ ipython_kernel_config.py
@@ -0,0 +1,3 @@
+c = get_config()
+
+c.IPKernelApp.matplotlib = 'inline'
---
+++ start-singleuser.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+# pull examples notebooks
+git clone https://github.com/met-office-lab/example-notebooks.git /usr/local/share/notebooks/Example\ Notebooks
+rm /usr/local/share/notebooks/Example\ Notebooks/LICENSE
+
+# start single user server
+notebook_arg=""
+if [ -n "${NOTEBOOK_DIR:+x}" ]
+then
+    notebook_arg="--notebook-dir=${NOTEBOOK_DIR}"
+fi
+
+exec jupyterhub-singleuser \
+  --port=8888 \
+  --ip=0.0.0.0 \
+  --user=$JPY_USER \
+  --cookie-name=$JPY_COOKIE_NAME \
+  --base-url=$JPY_BASE_URL \
+  --hub-prefix=$JPY_HUB_PREFIX \
+  --hub-api-url=$JPY_HUB_API_URL \
+  ${notebook_arg} \
+  $@
